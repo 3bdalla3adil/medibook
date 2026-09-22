27224000e10e19af5842d3590eb17d2b9a20d9db
abdulla bashir <abdullagozer@gmail.com>
2026-09-22 19:17:08 +0200

    feat(auth): add registration route

--- lib/app/router/routes.dart
+++ lib/app/router/routes.dart
@@ -1,6 +1,7 @@
 abstract final class Routes {
   static const splash = '/splash';
   static const login = '/login';
+  static const register = '/register';
 
   static const dashboard = '/';
   static const appointments = '/appointments';
