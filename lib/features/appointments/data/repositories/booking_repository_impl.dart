47b60e12a3effcec4597a0235f148ba14e711e4a
abdulla bashir <abdullagozer@gmail.com>
2026-09-22 13:44:08 +0200

    fix: resolve analyzer issues in appointment UI

--- lib/features/appointments/data/repositories/booking_repository_impl.dart
+++ lib/features/appointments/data/repositories/booking_repository_impl.dart
@@ -1,5 +1,5 @@
-import '../../../../core/error/result.dart';
 import '../../../../core/error/failure.dart';
+import '../../../../core/error/result.dart';
 import '../../../../core/network/api_endpoints.dart';
 import '../../domain/entities/booking_option.dart';
 import '../../domain/entities/booking_slot.dart';
