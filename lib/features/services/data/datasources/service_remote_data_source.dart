192fab21ee02f73a8f48d3bdb6bb61b2c920c28c
abdulla bashir <abdullagozer@gmail.com>
2026-09-23 12:10:41 +0200

    style: fix service datasource lints

--- lib/features/services/data/datasources/service_remote_data_source.dart
+++ lib/features/services/data/datasources/service_remote_data_source.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 
 import '../../../../core/network/api_endpoints.dart';
 import '../models/medical_service_dto.dart';
-
 abstract interface class ServiceRemoteDataSource {
   Future<List<MedicalServiceDto>> fetchServices({String? clinicId});
   Future<MedicalServiceDto?> fetchService(String id);
@@ -20,14 +19,16 @@ class DioServiceRemoteDataSource implements ServiceRemoteDataSource {
     );
     final data = response.data?['data'];
     if (data is! List) return const [];
-    return data.whereType<Map<String, dynamic>>()
-        .map(MedicalServiceDto.fromJson).toList(growable: false);
+    return data
+        .whereType<Map<String, dynamic>>()
+        .map(MedicalServiceDto.fromJson)
+        .toList(growable: false);
   }
 
   @override
   Future<MedicalServiceDto?> fetchService(String id) async {
     final response = await _dio.get<Map<String, dynamic>>(
-      ApiEndpoints.services + '/' + id,
+      ApiEndpoints.services + '/$id',
     );
     final data = response.data?['data'];
     return data is Map<String, dynamic> ? MedicalServiceDto.fromJson(data) : null;
