ac6161f8ccea742fe9cf9845488a8fb06c2ff700
abdulla bashir <abdullagozer@gmail.com>
2026-09-23 11:55:35 +0200

    fix(test): import logging level for demo integration test

--- integration_test/demo_login_flow_test.dart
+++ integration_test/demo_login_flow_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logging/logging.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:medibook/app/app.dart';
 import 'package:medibook/core/config/app_config.dart';
