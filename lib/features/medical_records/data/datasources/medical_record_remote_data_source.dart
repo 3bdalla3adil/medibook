79715223f349a61729c4fdda7241a9f18d21e3bc
abdulla bashir <abdullagozer@gmail.com>
2026-09-23 12:10:35 +0200

    style: fix medical record datasource lints

--- lib/features/medical_records/data/datasources/medical_record_remote_data_source.dart
+++ lib/features/medical_records/data/datasources/medical_record_remote_data_source.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 
 import '../models/medical_record_entry_dto.dart';
-
 abstract interface class MedicalRecordRemoteDataSource {
   Future<List<MedicalRecordEntryDto>> fetchEntries(String patientId);
 }
@@ -13,11 +12,13 @@ class DioMedicalRecordRemoteDataSource implements MedicalRecordRemoteDataSource 
   @override
   Future<List<MedicalRecordEntryDto>> fetchEntries(String patientId) async {
     final response = await _dio.get<Map<String, dynamic>>(
-      '/patients/' + patientId + '/medical-record',
+      '/patients/$patientId/medical-record',
     );
     final data = response.data?['data'];
     if (data is! List) return const [];
-    return data.whereType<Map<String, dynamic>>()
-        .map(MedicalRecordEntryDto.fromJson).toList(growable: false);
+    return data
+        .whereType<Map<String, dynamic>>()
+        .map(MedicalRecordEntryDto.fromJson)
+        .toList(growable: false);
   }
 }
