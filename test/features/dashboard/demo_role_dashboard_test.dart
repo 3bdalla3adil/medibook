28623066e56d2f1ba94906531fffcf7ccab11301
abdulla bashir <abdullagozer@gmail.com>
2026-09-23 16:55:09 +0200

    fix(test): satisfy analyzer for dashboard widget test

--- test/features/dashboard/demo_role_dashboard_test.dart
+++ test/features/dashboard/demo_role_dashboard_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-
 import 'package:medibook/features/auth/domain/entities/auth_user.dart';
 import 'package:medibook/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:medibook/features/dashboard/presentation/pages/role_dashboard_page.dart';
@@ -71,7 +70,7 @@ Future<void> _pumpDashboard(WidgetTester tester, AuthUser user) async {
   await tester.pumpWidget(
     BlocProvider<AuthBloc>.value(
       value: bloc,
-      child: MaterialApp(
+      child: const MaterialApp(
         locale: const Locale('ar'),
         supportedLocales: AppLocalizations.supportedLocales,
         localizationsDelegates: const [
