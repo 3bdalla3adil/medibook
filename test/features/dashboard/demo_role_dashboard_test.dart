ec7a63a4e98b06a7e588f08e0ef1bf543426b19c
abdulla bashir <abdullagozer@gmail.com>
2026-09-23 16:52:43 +0200

    fix(test): use package imports for demo dashboard tests

--- test/features/dashboard/demo_role_dashboard_test.dart
+++ test/features/dashboard/demo_role_dashboard_test.dart
@@ -4,10 +4,10 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-import '../../../lib/features/auth/domain/entities/auth_user.dart';
-import '../../../lib/features/auth/presentation/bloc/auth_bloc.dart';
-import '../../../lib/features/dashboard/presentation/pages/role_dashboard_page.dart';
-import '../../../lib/l10n/gen/app_localizations.dart';
+import 'package:medibook/features/auth/domain/entities/auth_user.dart';
+import 'package:medibook/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:medibook/features/dashboard/presentation/pages/role_dashboard_page.dart';
+import 'package:medibook/l10n/gen/app_localizations.dart';
 
 class _MockAuthBloc extends Mock implements AuthBloc {}
 
