7b32322250edac806ff939320516198835c2dbc6
abdulla bashir <abdullagozer@gmail.com>
2026-09-24 22:07:27 +0200

    fix(test): sort package directives alphabetically

--- test/features/auth/presentation/login_routing_test.dart
+++ test/features/auth/presentation/login_routing_test.dart
@@ -5,11 +5,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:medibook/features/auth/domain/entities/auth_user.dart';
 import 'package:medibook/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:medibook/features/auth/presentation/pages/login_page.dart';
 import 'package:medibook/l10n/gen/app_localizations.dart';
+import 'package:mocktail/mocktail.dart';
 
 class _MockAuthBloc extends Mock implements AuthBloc {}
 
